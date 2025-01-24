8710fd211d63c81240813c89d8682b0429bad5cf
khalilkhemiri <126549623+khalilkhemiri@users.noreply.github.com>
2025-01-24 15:32:38 +0100

    Update App.py

--- soft-ui-dashboard-main/App.py
+++ soft-ui-dashboard-main/App.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 # MongoDB connection
-client = MongoClient("mongodb+srv://khalilleo:khalilleo@cluster0.bfwkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", serverSelectionTimeoutMS=5000)
+client = MongoClient('mongodb+srv://khalilleo:khalilleo@cluster0.bfwkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', serverSelectionTimeoutMS=5000)
 db = client["khalilleo"]
 collection_cotisations = db["cotisations"]
 collection_caisse_membre = db["caisse_membre"]
